4e17e11924590815685800165a2ca54a99bb30c1
Jakub Tomczak <jakub.tomczak@live.com>
2018-10-09 12:20:51 +0200

    Add model building.

---
+++ R/model.R
@@ -0,0 +1,153 @@
+#### BUILDING MODEL
+
+#' @export
+buildModel <- function(problem, minEpsilon = 1e-4, method="utamp-1") { # includeEpsilonAsVariable,
+  if(! (method %in% c("uta", "utamp-1", "utamp-2")))
+  {
+    stop("Method must be on of the following: `uta`, `utamp-1`, `utamp-2`")
+  }
+  nrAlternatives <- nrow(problem$preferences)
+  nrCriteria <- ncol(problem$preferences)
+
+  firstChPointVariableIndex <- c(1)
+  chPoints <- c()
+
+  #calculate characteristic points indices
+  for (j in seq_len(nrCriteria)) {
+    numberOfCharacteristicPoints <- problem$characteristicPoints[j]
+
+    if (numberOfCharacteristicPoints == 0) { #jeżeli nie wiadomo ile jest punktów charakterystycznych to załóż, że jest tyle ile jest kryteriów de facto
+      numberOfCharacteristicPoints <- length(problem$preferences[[j]])
+    }
+
+    if (j != nrCriteria) { #jeżeli nie jesteśmy w ostatnim przejściu pętli
+      #kolejny punkt charakterystyczny to aktualny + liczba punktów charakterystycznych dla tego kryterium -1
+      firstChPointVariableIndex[j + 1] <- firstChPointVariableIndex[j] + numberOfCharacteristicPoints - 1
+    }
+
+    chPoints[j] <- numberOfCharacteristicPoints
+  }
+
+  numberOfVariables <- firstChPointVariableIndex[length(firstChPointVariableIndex)] + chPoints[nrCriteria] - 2
+
+  #preferences to model variables used in solution
+  preferencesToModelVariables <- createPreferencesToModelVariables(problem, firstChPointVariableIndex)
+
+  # epsilon index
+  numberOfVariables <- numberOfVariables + 1
+  epsilonIndex <- numberOfVariables
+
+  # constraints
+  ## sum to 1
+  lhs <- rep(0, numberOfVariables)
+
+  for (j in seq_len(nrCriteria)) {
+    if (problem$criteria[j] == 'g')
+      lhs[firstChPointVariableIndex[j] + chPoints[j] - 2] <- 1
+    else
+      lhs[firstChPointVariableIndex[j]] <- 1
+  }
+
+  constraints <- list(lhs = lhs, dir = "==", rhs = 1)
+
+  ## monotonicity of vf
+  for (j in seq_len(nrCriteria)) {
+    for (k in seq_len(chPoints[j] - 2)) {
+      lhs <- rep(0, numberOfVariables)
+      rhs <- 0
+
+      if (problem$criteria[j] == "g") {
+        lhs[firstChPointVariableIndex[j] + k - 1] <- 1
+        lhs[firstChPointVariableIndex[j] + k] <- -1
+      } else {
+        lhs[firstChPointVariableIndex[j] + k - 1] <- -1
+        lhs[firstChPointVariableIndex[j] + k] <- 1
+      }
+
+      if (problem$strictVF) {
+        lhs[epsilonIndex] <- 1
+      }
+
+      constraints <- combineConstraints(constraints,
+                                        list(lhs = lhs, dir = "<=", rhs = rhs))
+    }
+
+    lhs <- rep(0, numberOfVariables)
+    rhs <- 0
+    if (problem$criteria[j] == 'g')
+      lhs[firstChPointVariableIndex[j]] <- -1
+    else
+      lhs[firstChPointVariableIndex[j] + chPoints[j] - 2] <- -1
+
+    if (problem$strictVF) {
+      lhs[epsilonIndex] <- 1
+    }
+    constraints <- combineConstraints(constraints,
+                                      list(lhs = lhs, dir = "<=", rhs = rhs))
+  }
+
+  constraints$types <- rep("C", numberOfVariables) #continous
+
+  ## building model
+  model <- list(
+    constraints = constraints,
+    firstChPointVariableIndex = firstChPointVariableIndex,
+    epsilonIndex = epsilonIndex,
+    chPoints = chPoints,
+    preferencesToModelVariables = preferencesToModelVariables,
+    criterionPreferenceDirection = problem$criteria,
+    prefInfoToConstraints = list(),
+    generalVF = problem$characteristicPoints == 0,
+    minEpsilon = minEpsilon
+  )
+
+  # preference information
+
+  ## assignment examples
+
+  prefInfoIndex <- 1
+
+  if (is.matrix(problem$strongPreference)) {
+    for (k in seq_len(nrow(problem$strongPreference))) {
+      alternative <- problem$strongPreference[k, 1]
+      referenceAlternative <- problem$strongPreference[k, 2]
+
+      model$constraints <- combineConstraints(model$constraints,
+                                              buildPairwiseComparisonConstraint(alternative, referenceAlternative,
+                                                                                model, type = "strongPreference", method))
+
+      model$prefInfoToConstraints[[prefInfoIndex]] <- nrow(model$constraints$lhs)
+      prefInfoIndex <- prefInfoIndex + 1
+    }
+  }
+
+  if (is.matrix(problem$weakPreference)) {
+    for (k in seq_len(nrow(problem$weakPreference))) {
+      alternative <- problem$weakPreference[k, 1]
+      referenceAlternative <- problem$weakPreference[k, 2]
+
+      model$constraints <- combineConstraints(model$constraints,
+                                              buildPairwiseComparisonConstraint(alternative, referenceAlternative,
+                                                                                model, type = "weakPreference", method))
+
+      model$prefInfoToConstraints[[prefInfoIndex]] <- nrow(model$constraints$lhs)
+      prefInfoIndex <- prefInfoIndex + 1
+    }
+  }
+
+  if (is.matrix(problem$indifference)) {
+    for (k in seq_len(nrow(problem$indifference))) {
+      alternative <- problem$indifference[k, 1]
+      referenceAlternative <- problem$indifference[k, 2]
+
+      model$constraints <- combineConstraints(model$constraints,
+                                              buildPairwiseComparisonConstraint(alternative, referenceAlternative,
+                                                                                model, type = "indifference", method))
+
+      model$prefInfoToConstraints[[prefInfoIndex]] <- nrow(model$constraints$lhs)
+      prefInfoIndex <- prefInfoIndex + 1
+    }
+  }
+
+  return (model)
+}
---
+++ R/modelHelpers.R
@@ -0,0 +1,181 @@
+#### HELPERS
+createPreferencesToModelVariables <- function(problem, firstChPointVariableIndex)
+{
+  nrAlternatives <- nrow(problem$preferences)
+  nrCriteria <- ncol(problem$preferences)
+
+  preferencesToModelVariables <- replicate(nrCriteria, replicate(nrAlternatives, list()))
+  for (j in seq_len(nrCriteria)) {
+    minimalValue <- min(problem$preferences[,j]) #minimal value on this criterium
+    maximalValue <- max(problem$preferences[,j]) #maximal value on this criterium
+    direction <- problem$criteria[j]  #
+    mostValuableValue <- if(direction == 'c') minimalValue else maximalValue
+    leastValuableValue <- if(direction == 'c') maximalValue else minimalValue
+
+    if (problem$characteristicPoints[j] == 0) {
+      stop("Not implemented yet. Set characteristic points explicitly.")
+    } else {
+      numberOfCharacteristicPoints <- problem$characteristicPoints[j]
+      intervalLength <- (maximalValue - minimalValue) / (numberOfCharacteristicPoints - 1);
+      coeff <- 1.0 / intervalLength;
+
+      for (i in seq_len(nrAlternatives)) {
+        value <- problem$preferences[i, j]
+
+        if(value == mostValuableValue) #epsilion?
+        {
+          #don't know why this offset is used
+          offset <- if(direction == "c") 0 else numberOfCharacteristicPoints - 2
+          preferencesToModelVariables[[i,j]][1] <- c(firstChPointVariableIndex[j] + offset, 1.0)
+
+          #least valuable alternative has its score equal to 0, that is why we ommit this this special case
+          #least valuable alternative has an empty list in perfToModelVariable matrix
+        } else if(value != leastValuableValue) {
+          #get the index of a chunk of intervalLength length
+          lowerChPointIndex <- floor((value-minimalValue)*coeff) #floor { (V-Min)/(Max-Min) * (Num-1) }
+
+          #get the bounds of this chunk
+          lowerValue = minimalValue + intervalLength * lowerChPointIndex
+          upperValue = minimalValue + intervalLength * (lowerChPointIndex + 1)
+
+          lowerCoeff <- 0.0
+          upperCoeff <- 0.0
+
+          if (value <= lowerValue) {
+            # comp accuracy
+            lowerCoeff = 1.0
+            upperCoeff = 0.0
+          } else if (value >= upperValue) {
+            # comp accuracy
+            lowerCoeff = 0.0
+            upperCoeff = 1.0
+          } else if(direction == "g") {
+            #gain type
+            lowerCoeff = (lowerValue - value) / (upperValue - lowerValue) + 1.0
+            upperCoeff = (value - lowerValue) / (upperValue - lowerValue)
+          } else {
+            #cost type
+            lowerCoeff = (upperValue - value) / (upperValue - lowerValue)
+            upperCoeff = (value - upperValue) / (upperValue - lowerValue) + 1.0
+          }
+
+          if(direction == "g")
+          {
+            #gain type
+            if(lowerChPointIndex > 0)
+            {
+              preferencesToModelVariables[[i, j]][[1]] = c(firstChPointVariableIndex[j] + lowerChPointIndex - 1, lowerCoeff)
+              preferencesToModelVariables[[i, j]][[2]] = c(firstChPointVariableIndex[j] + lowerChPointIndex, upperCoeff)
+            }
+            else
+            {
+              preferencesToModelVariables[[i, j]][[1]] = c(firstChPointVariableIndex[j] + lowerChPointIndex, upperCoeff)
+            }
+          }
+          else
+          {
+            #cost type
+            if(lowerChPointIndex < numberOfCharacteristicPoints - 2)
+            {
+              preferencesToModelVariables[[i, j]][[1]] = c(firstChPointVariableIndex[j] + lowerChPointIndex, lowerCoeff)
+              preferencesToModelVariables[[i, j]][[2]] = c(firstChPointVariableIndex[j] + lowerChPointIndex + 1, upperCoeff)
+            }
+            else
+            {
+              preferencesToModelVariables[[i, j]][[1]] = c(firstChPointVariableIndex[j] + lowerChPointIndex, lowerCoeff)
+            }
+          }
+        }
+      }
+    }
+  }
+  return(preferencesToModelVariables)
+}
+
+
+buildPairwiseComparisonConstraint <- function(alternative, referenceAlternative, model, type, method) {
+  stopifnot(type %in% c("weakPreference", "strongPreference", "indifference"))
+  #-1 to remove epsilion index
+  variables <- ncol(model$constraints$lhs) - 1
+  lhs <- ua(referenceAlternative, ncol(model$constraints$lhs), model$preferencesToModelVariables) - ua(alternative, ncol(model$constraints$lhs), model$preferencesToModelVariables)
+  method
+  if(method == "uta")
+  {
+    #for UTA object function
+    additionalLHSMatrix <- matrix(rep(0, 2*variables^2), ncol=2*variables)
+    lhs <- merge(lhs, additionalLHSMatrix)
+  }
+  dir <- "<="
+  rhs <- 0
+
+  if (type == "strongPreference") {
+    if (is.null(model$epsilonIndex)) {
+      rhs <- -model$minEpsilon
+    } else {
+      lhs[model$epsilonIndex] <- 1
+    }
+  } else if (type == "indifference") {
+    dir <- "=="
+  }
+
+  return (list(lhs = lhs, dir = dir, rhs = rhs))
+}
+
+
+addVarialbesToModel <- function(constraints, variables) {
+  for (var in variables)
+    constraints$lhs <- cbind(constraints$lhs, 0)
+  constraints$types <- c(constraints$types, variables)
+  return (constraints)
+}
+
+combineConstraints <- function(...) {
+  allConst <- list(...)
+
+  lhs <- c()
+  dir <- c()
+  rhs <- c()
+  types <- c()
+
+  for (const in allConst) {
+    if (!is.null(const)) {
+      lhs <- rbind(lhs, const$lhs)
+      dir <- c(dir, const$dir)
+      rhs <- c(rhs, const$rhs)
+      types <- c(types, const$types)
+    }
+  }
+
+  return (list(lhs = lhs, dir = dir, rhs = rhs, types = types))
+}
+
+removeConstraints <- function(allConst, constraintsToRemoveIndices) {
+  return (list(lhs = allConst$lhs[-c(constraintsToRemoveIndices), ],
+               dir = allConst$dir[-c(constraintsToRemoveIndices)],
+               rhs = allConst$rhs[-c(constraintsToRemoveIndices)],
+               types = allConst$types))
+}
+
+
+ua <- function(alternative, nrVariables, preferencesToModelVariables) {
+  res <- rep(0, nrVariables)
+
+  for (j in seq_len(ncol(preferencesToModelVariables))) {
+    for (k in seq_len(length(preferencesToModelVariables[[alternative, j]]))) {
+      res[preferencesToModelVariables[[alternative, j]][[k]][1]] <- preferencesToModelVariables[[alternative, j]][[k]][2]
+    }
+  }
+
+  return (res)
+}
+
+eliminateEpsilon <- function(model) {
+  stopifnot(!is.null(model$epsilonIndex))
+
+  model$constraints$rhs <- model$constraints$rhs - model$constraints$lhs[, model$epsilonIndex] * model$minEpsilon
+  model$constraints$lhs <- model$constraints$lhs[, -c(model$epsilonIndex)]
+  model$constraints$types <- model$constraints$types[-c(model$epsilonIndex)]
+  model$epsilonIndex <- NULL
+
+  return (model)
+}
